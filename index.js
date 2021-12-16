2a7de9a5fcadec76b876d37a5ec180bcbda46143
Alexis THOMAS <fr.alexisthomas@gmail.com>
2021-12-17 00:59:18 +0100

    override pre-release version to snapshot: implement, test and coverage

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const { Plugin } = require('release-it');
+const VersionPlugin = require('release-it/lib/plugin/version/Version');
+const semver = require('semver');
 
-class SnapshotPreReleaseVersionPlugin extends Plugin {}
+class SnapshotPreReleaseVersionPlugin extends VersionPlugin {
+  static isEnabled(options) {
+    return options !== false;
+  }
+
+  incrementVersion(...args) {
+    const nextVersion = super.incrementVersion(...args);
+    const parsedVersion = semver.parse(nextVersion);
+    if (parsedVersion && parsedVersion.prerelease.length) {
+      if (parsedVersion.prerelease.length === 1 && typeof parsedVersion.prerelease[0] === 'number') {
+        parsedVersion.prerelease = ['SNAPSHOT'];
+        return parsedVersion.format();
+      } else if (typeof parsedVersion.prerelease[parsedVersion.prerelease.length - 1] === 'number') {
+        parsedVersion.prerelease.pop();
+        return parsedVersion.format();
+      }
+    }
+    return null;
+  }
+}
 
 module.exports = SnapshotPreReleaseVersionPlugin;
--- test.js
+++ test.js
@@ -1,9 +1,42 @@
 const test = require('bron');
 const assert = require('assert').strict;
-const { factory, runTasks } = require('release-it/test/util');
+const sinon = require('sinon');
+const { factory } = require('release-it/test/util');
 const Plugin = require('.');
+const VersionPlugin = require('release-it/lib/plugin/version/Version');
 
-test('should not throw', async t => {
+test('isEnabled true', (t) => {
+  assert.equal(Plugin.isEnabled(), true);
+});
+
+test('isEnabled false if options=false', (t) => {
+  assert.equal(Plugin.isEnabled(false), false);
+});
+
+const superIncrementVersionStubReturns = (value) => {
+  if (!this.superIncrementVersionStub) {
+    this.superIncrementVersionStub = sinon.stub(VersionPlugin.prototype, 'incrementVersion');
+    this.superIncrementVersionStubCallCount = 0;
+  }
+  this.superIncrementVersionStub.onCall(this.superIncrementVersionStubCallCount++).returns(value);
+};
+
+test('incrementVersion null', (t) => {
   const plugin = factory(Plugin);
-  await assert.doesNotReject(runTasks(plugin));
+  superIncrementVersionStubReturns(undefined);
+  assert.equal(plugin.incrementVersion(), null);
+  superIncrementVersionStubReturns('1.2.4');
+  assert.equal(plugin.incrementVersion(), null);
+  superIncrementVersionStubReturns('1.2.4-beta');
+  assert.equal(plugin.incrementVersion(), null);
+  superIncrementVersionStubReturns('1.2.4-alpha.beta-0');
+  assert.equal(plugin.incrementVersion(), null);
+});
+
+test('incrementVersion snapshot', (t) => {
+  const plugin = factory(Plugin);
+  superIncrementVersionStubReturns('1.2.4-0');
+  assert.equal(plugin.incrementVersion(), '1.2.4-SNAPSHOT');
+  superIncrementVersionStubReturns('1.2.4-beta.0');
+  assert.equal(plugin.incrementVersion(), '1.2.4-beta');
 });
